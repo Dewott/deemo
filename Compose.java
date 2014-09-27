e2eba6ee4b19731320a00199bb6f0c687fbff685
Dewott <413405663@qq.com>
2014-09-27 21:15:23 +0800

    2014-09-27 Update

--- Compose.java
+++ Compose.java
@@ -8,93 +8,104 @@ import java.awt.image.*;
 import javax.media.*;
 import javazoom.jl.converter.*;
 
-public class Compose{
-  LinkedList<Note> notes=new LinkedList<Note>();
-  String name=null;
-  double ar=600;
-  int time=0;
-  public Compose(String name,String diff,int speed) throws Exception{
-    this.name=name;
-    new ComposeReader(name,diff);
-	double t[]=new double[]{8000,4000,2000,1600,1200,900,600,300,100};
-	this.ar=t[speed-1];
-  }
-  class ComposeReader{
-    public ComposeReader(String name,String diff) throws Exception{
-	  BufferedReader r=new BufferedReader(new FileReader("TextAsset/"+name+"."+diff+".json.txt"));
-	  String str="";
-	  String s=r.readLine();
-	  while(s!=null){
-	    str+=s;
-		s=r.readLine();
-	  }
-	  r.close();
-	  
-	  JSONObject obj=JSONObject.fromObject(str);
-	  JSONArray arr=JSONArray.fromObject(obj.get("notes"));
-	  for(Object tobj:arr){
-	    JSONObject nobj=JSONObject.fromObject(tobj);
-		double x=nobj.optDouble("pos",0);
-		int time=(int)(nobj.getDouble("_time")*1000);
-		double size=nobj.getDouble("size");
-		Note n=new Note(Compose.this,x,time,size);
-		notes.add(n);
-		
-		JSONArray arr2=nobj.optJSONArray("sounds");
-		if(arr2!=null){
-		  for(Object tobj2:arr2){
-		    JSONObject sobj=JSONObject.fromObject(tobj2);
-			int d=(int)(sobj.getDouble("d")*1000);
-			int p=sobj.getInt("p");
-			int v=sobj.getInt("v");
-			double w=sobj.optDouble("w",0);
-			n.addSound(new Sound(d,p,v,w));
-		  }
-		}
-	  }
-	  
-	  arr=JSONArray.fromObject(obj.get("links"));
-	  for(Object tobj:arr){
-	    JSONObject link=JSONObject.fromObject(tobj);
-		JSONArray arr2=JSONArray.fromObject(link.get("notes"));
-		
-		for(Object tobj2:arr2){
-		  JSONObject note=JSONObject.fromObject(tobj2);
-		  int p=Integer.parseInt(note.getString("$ref"));
-		  notes.get(p-1).setLink(true);
-		}
-	  }
+public class Compose {
+	LinkedList<Note> notes = new LinkedList<Note>();
+	String name = null;
+	double ar = 600;
+	int time = 0;
+
+	public Compose(String name, String diff, int speed) throws Exception {
+		this.name = name;
+		new ComposeReader(name, diff);
+		double t[] = new double[] { 8000, 4000, 2000, 1600, 1200, 900, 600,
+				300, 100 };
+		this.ar = t[speed - 1];
 	}
-  }
-  public void start(Graphics2D g){
-    Player p=null;
-    try{
-	  new Converter().convert("AudioClip/"+name+".mp3","temp.wav");
-      p=Manager.createRealizedPlayer(new File("temp.wav").toURI().toURL());
-	}catch(Exception e){}
-    p.start();
-	p.getGainControl().setLevel(0.4f);
-	BufferedImage buf=new BufferedImage(720,480,BufferedImage.TYPE_INT_ARGB);
-	Graphics2D gg=(Graphics2D)buf.getGraphics();
-	gg.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
-	gg.setStroke(new BasicStroke(5));
-	while(true){
-	  gg.setColor(Color.WHITE);
-	  gg.fillRect(0,0,720,480);
-	  gg.setColor(Color.BLACK);
-	  gg.drawLine(0,400,720,400);
-	  time=(int)(p.getMediaTime().getSeconds()*1000);
-	  LinkedList<Note> del=new LinkedList<Note>();
-	  for(Note n:notes){
-	    if(time>n.time) del.add(n);
-		else
-	      if((time<=n.time+ar)&&(time>=n.time-ar)) n.paint(gg);
-	  }
-	  notes.removeAll(del);
-	  g.drawImage(buf,0,0,null);
-	  try{
-	    Thread.sleep(10);
-	  }catch(Exception e){}
+
+	class ComposeReader {
+		public ComposeReader(String name, String diff) throws Exception {
+			BufferedReader r = new BufferedReader(new FileReader("TextAsset/"
+					+ name + "." + diff + ".json.txt"));
+			String str = "";
+			String s = r.readLine();
+			while (s != null) {
+				str += s;
+				s = r.readLine();
+			}
+			r.close();
+
+			JSONObject obj = JSONObject.fromObject(str);
+			JSONArray arr = JSONArray.fromObject(obj.get("notes"));
+			for (Object tobj : arr) {
+				JSONObject nobj = JSONObject.fromObject(tobj);
+				double x = nobj.optDouble("pos", 0);
+				int time = (int) (nobj.getDouble("_time") * 1000);
+				double size = nobj.getDouble("size");
+				Note n = new Note(Compose.this, x, time, size);
+				notes.add(n);
+
+				JSONArray arr2 = nobj.optJSONArray("sounds");
+				if (arr2 != null) {
+					for (Object tobj2 : arr2) {
+						JSONObject sobj = JSONObject.fromObject(tobj2);
+						int d = (int) (sobj.getDouble("d") * 1000);
+						int p = sobj.getInt("p");
+						int v = sobj.getInt("v");
+						double w = sobj.optDouble("w", 0);
+						n.addSound(new Sound(d, p, v, w));
+					}
+				}
+			}
+
+			arr = JSONArray.fromObject(obj.get("links"));
+			for (Object tobj : arr) {
+				JSONObject link = JSONObject.fromObject(tobj);
+				JSONArray arr2 = JSONArray.fromObject(link.get("notes"));
+
+				for (Object tobj2 : arr2) {
+					JSONObject note = JSONObject.fromObject(tobj2);
+					int p = Integer.parseInt(note.getString("$ref"));
+					notes.get(p - 1).setLink(true);
+				}
+			}
+		}
 	}
-  }
+
+	public void start(Graphics2D g) {
+		Player p = null;
+		try {
+			new Converter().convert("AudioClip/" + name + ".mp3", "temp.wav");
+			p = Manager.createRealizedPlayer(new File("temp.wav").toURI()
+					.toURL());
+		} catch (Exception e) {
+		}
+		p.start();
+		p.getGainControl().setLevel(0.4f);
+		BufferedImage buf = new BufferedImage(720, 480,
+				BufferedImage.TYPE_INT_ARGB);
+		Graphics2D gg = (Graphics2D) buf.getGraphics();
+		gg.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		gg.setStroke(new BasicStroke(5));
+		while (true) {
+			gg.setColor(Color.WHITE);
+			gg.fillRect(0, 0, 720, 480);
+			gg.setColor(Color.BLACK);
+			gg.drawLine(0, 400, 720, 400);
+			time = (int) (p.getMediaTime().getSeconds() * 1000);
+			LinkedList<Note> del = new LinkedList<Note>();
+			for (Note n : notes) {
+				if (time > n.time)
+					del.add(n);
+				else if ((time <= n.time + ar) && (time >= n.time - ar))
+					n.paint(gg);
+			}
+			notes.removeAll(del);
+			g.drawImage(buf, 0, 0, null);
+			try {
+				Thread.sleep(10);
+			} catch (Exception e) {
+			}
+		}
+	}
 }
--- Sound.java
+++ Sound.java
@@ -1,34 +1,40 @@
 package deemo;
+
 import javax.sound.midi.*;
+
 //-classpath rt.jar
-public class Sound implements Runnable{
-  int duration=0;
-  int pitch=0;
-  int volume=0;
-  double wane=0;
-  static MidiChannel c=null;
-  static long latency=0;
-  static{
-    try{
-      Synthesizer s=MidiSystem.getSynthesizer();
-	  latency=s.getLatency()/1000;
-      Instrument ins[]=s.getAvailableInstruments();
-	  s.loadInstrument(ins[0]);
-	  s.open();
-	  c=s.getChannels()[0];
-	}catch(Exception e){}
-  }
-  public Sound(int duration,int pitch,int volume,double wane){
-    this.duration=duration;
-	this.pitch=pitch;
-	this.volume=volume;
-	this.wane=wane;
-  }
-  public void run(){
-    c.noteOn(pitch,volume);
-	try{
-	  Thread.sleep(duration);
-	}catch(Exception e){}
-	c.noteOff(pitch,0);
-  }
+public class Sound implements Runnable {
+	int duration = 0;
+	int pitch = 0;
+	int volume = 0;
+	double wane = 0;
+	static MidiChannel c = null;
+	static long latency = 0;
+	static {
+		try {
+			Synthesizer s = MidiSystem.getSynthesizer();
+			latency = s.getLatency() / 1000;
+			Instrument ins[] = s.getAvailableInstruments();
+			s.loadInstrument(ins[0]);
+			s.open();
+			c = s.getChannels()[0];
+		} catch (Exception e) {
+		}
+	}
+
+	public Sound(int duration, int pitch, int volume, double wane) {
+		this.duration = duration;
+		this.pitch = pitch;
+		this.volume = volume;
+		this.wane = wane;
+	}
+
+	public void run() {
+		c.noteOn(pitch, volume);
+		try {
+			Thread.sleep(duration);
+		} catch (Exception e) {
+		}
+		c.noteOff(pitch, 0);
+	}
 }
